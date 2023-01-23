e01d5a3f4b714c606cd9d4c9f3ea2144e1da4e54
Proly <92342752+Prolynho@users.noreply.github.com>
2023-01-23 09:54:18 -0300

    Update ex010.py

--- Exercícios/ex010.py
+++ Exercícios/ex010.py
@@ -1,3 +1,7 @@
+'''
+Crie um programa que leia quanto dinheiro uma pessoa tem 
+na carteira e mostre quantos dólares ela pode comprar
+'''
 real = float(input('How many money? R$'))
 dollar = float(real / 5.45)
 print('You have US${}{:.2f}{}'.format('\033[1:32m', dollar, '\033[m'))
