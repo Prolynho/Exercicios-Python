6e093fa2d029b721c3237ed27c8f99ee662c9bf6
Proly <92342752+Prolynho@users.noreply.github.com>
2023-01-23 10:15:20 -0300

    Update ex028.py

--- Exercícios/ex028.py
+++ Exercícios/ex028.py
@@ -1,3 +1,8 @@
+'''
+Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5
+e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador.
+O programa deverá escrever na tela se o usuário venceu ou perdeu.
+'''
 from random import randint
 number = randint(0, 5)
 guess = int(input("Insert a number (0 to 5) and I'll try to guess: "))
