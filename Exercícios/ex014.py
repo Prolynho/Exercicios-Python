eabecf9431780155182d27633acea8e02da20e74
Proly <92342752+Prolynho@users.noreply.github.com>
2023-01-23 09:59:42 -0300

    Update ex014.py

--- Exercícios/ex014.py
+++ Exercícios/ex014.py
@@ -1,3 +1,7 @@
+'''
+Conversor de temperaturas: escreva um programa 
+que converta uma temperatura digitada em ºC para ºF
+'''
 celsius = float(input('Insert the value in °C: '))
 fahrenheit = (celsius * 9/5) + celsius
 print('converting the value to fahrenheit is equivalent to {:.2f}°F'.format(fahrenheit))
