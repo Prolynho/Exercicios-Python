e71fa8f636bf5f67f8eacc5151b48c715ae40466
Proly <92342752+Prolynho@users.noreply.github.com>
2023-01-23 09:56:05 -0300

    Update ex011.py

--- Exercícios/ex011.py
+++ Exercícios/ex011.py
@@ -1,3 +1,8 @@
+'''
+Faça um programa que leia a largura e a algura de uma parede em metros, 
+calcule a sua área e a quantidade de tinta necessária para pintá-la, 
+sabendo que cada litro de tinta pinta uma área de 2m²
+'''
 width = float(input('Insert the width: '))
 height = float(input('Insert the height: '))
 area = (width * height)
