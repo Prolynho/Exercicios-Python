2c2e3a2f37074791ebf9d14a69b13633f0c10cb1
Proly <92342752+Prolynho@users.noreply.github.com>
2023-01-23 10:02:52 -0300

    Update ex016.py

--- Exercícios/ex016.py
+++ Exercícios/ex016.py
@@ -1,3 +1,7 @@
+'''
+Desafio 16: Crie um programa que leia um número real 
+qualquer pelo teclado e mostre na tela a sua porção inteira.
+'''
 from math import trunc
 num = float(input('Insert value: '))
 truncate = trunc(num)
