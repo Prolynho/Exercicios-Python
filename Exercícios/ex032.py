cd75a4a35047b64e10db778f5b64910c3d4eaeae
Proly <92342752+Prolynho@users.noreply.github.com>
2023-01-23 10:18:00 -0300

    Update ex032.py

--- Exercícios/ex032.py
+++ Exercícios/ex032.py
@@ -1,3 +1,4 @@
+# Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO
 from datetime import date
 leapYear = int(input('Insert a year (insert 0 to analyze the actual date: '))
 if leapYear == 0:
