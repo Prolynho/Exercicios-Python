bb4627fff12294ce8345c0862a785aa71a448eee
Proly <92342752+Prolynho@users.noreply.github.com>
2023-01-23 10:07:18 -0300

    Update ex020.py

--- Exercícios/ex020.py
+++ Exercícios/ex020.py
@@ -1,3 +1,7 @@
+'''
+O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos
+Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+'''
 from random import shuffle
 names = [input('1st name: '), input('2nd name: '), input('3rd name: '), input('4th name: ')]
 shuffle(names)
