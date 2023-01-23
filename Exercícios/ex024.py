8ed8a74a44b851b2dc3bfde17177cfd718423d69
Proly <92342752+Prolynho@users.noreply.github.com>
2023-01-23 10:11:59 -0300

    Update ex024.py

--- Exercícios/ex024.py
+++ Exercícios/ex024.py
@@ -1,3 +1,4 @@
+# Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com a palavra "Santo"
 city = input('Insert your city: ').strip()
 print("""So, your city is {}.
 Have Santo: {}
