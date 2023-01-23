f72a461bfa877216bb6a946771331f914a809b5e
Proly <92342752+Prolynho@users.noreply.github.com>
2023-01-23 10:06:47 -0300

    Update ex019.py

--- Exercícios/ex019.py
+++ Exercícios/ex019.py
@@ -1,3 +1,7 @@
+'''
+Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
+Faça um programa que ajude ele, lendo o nome deles e escrevendo o do escolhido.
+'''
 from random import choice
 names = [input('1st name: '), input('2nd name: '), input('3rd name: '), input('4th name: ')]
 random = choice(names)
