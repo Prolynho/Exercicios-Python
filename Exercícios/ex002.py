279634f31b69caf9bf3ecd3556f4995f8c15afe1
Proly <92342752+Prolynho@users.noreply.github.com>
2023-01-23 09:43:11 -0300

    Update ex002.py

--- Exercícios/ex002.py
+++ Exercícios/ex002.py
@@ -1,2 +1,3 @@
+# Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado
 nome = input('Digite seu nome: ')
 print('É um prazer te conhecer, {}{}{}!'.format('\033[1:34m', nome, '\033[m'))
