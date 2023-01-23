356f415353212266f6594e1336763ad698de8e32
Proly <92342752+Prolynho@users.noreply.github.com>
2023-01-23 10:16:46 -0300

    Update ex031.py

--- Exercícios/ex031.py
+++ Exercícios/ex031.py
@@ -1,3 +1,8 @@
+'''
+Desenvolva um programa que pergunte a distância de uma viagem em Km.
+Calcule o preço da passagem, cobrando R$ 0,50 por Km para viagens de até 200km
+e R$ 0,45 para viagens mais longas
+'''
 travel = float(input('How far do you want to travel (KM)? '))
 if travel <= 200:
     print('You will have to pay R${:.2f}'.format(travel * 0.50))
