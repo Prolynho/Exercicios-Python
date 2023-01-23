c79567ede27bdab615ecef097595d989a1e25388
Proly <92342752+Prolynho@users.noreply.github.com>
2023-01-23 10:19:01 -0300

    Update ex034.py

--- Exercícios/ex034.py
+++ Exercícios/ex034.py
@@ -1,3 +1,8 @@
+'''
+Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento
+Para salários superiores a R$ 1.250,00, calcule um aumento de 10%
+Para os inferiores ou iguais, o aumento é de R$ 15%.
+'''
 salary = float(input('How many is your salary? '))
 if salary >= 1250:
     newSalary = salary + (salary * 10/100)
