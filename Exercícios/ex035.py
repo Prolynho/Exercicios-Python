772587b3b414c92a7c82648a30978b291aaf3f9a
Proly <92342752+Prolynho@users.noreply.github.com>
2023-01-23 10:23:41 -0300

    Update ex035.py

--- Exercícios/ex035.py
+++ Exercícios/ex035.py
@@ -1,3 +1,7 @@
+'''
+Desenvolva um programa que leia o comprimento de três retas
+e diga ao usuário se elas podem ou não formar um triângulo
+'''
 a = int(input('Insert the first segment: '))
 b = int(input('Insert the second segment: '))
 c = int(input('Insert the third segment: '))
