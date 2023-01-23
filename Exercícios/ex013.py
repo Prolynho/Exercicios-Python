da9d4a99a10b74a922c7e22c34182f9b2ee19ae3
Proly <92342752+Prolynho@users.noreply.github.com>
2023-01-23 09:59:06 -0300

    Update ex013.py

--- Exercícios/ex013.py
+++ Exercícios/ex013.py
@@ -1,3 +1,6 @@
+'''Faça um algoritmo que leia o salário de um funcionário 
+e mostre seu novo salário, com 15% de aumento
+''
 wage = float(input('Wage: '))
 newWage = wage + (wage * 15/100)
 print('New wage: {:.2f}'.format(newWage))
