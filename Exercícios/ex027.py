8e2d41b5cd82ac64ca4f4db4ee6862a50cca3975
Proly <92342752+Prolynho@users.noreply.github.com>
2023-01-23 10:14:35 -0300

    Update ex027.py

--- Exercícios/ex027.py
+++ Exercícios/ex027.py
@@ -1,3 +1,7 @@
+'''
+Faça um programa que leia o nome completo de uma pessoa, 
+mostrando em seguida o primeiro e o último nome separadamente
+'''
 name = input('Insert your name: ')
 print("""So, your name is {}
 First Name: {}
