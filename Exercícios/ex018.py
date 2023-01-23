2a661fe8cb6675afc44fdf1efe4b4fd7a68cdaf2
Proly <92342752+Prolynho@users.noreply.github.com>
2023-01-23 10:05:14 -0300

    Update ex018.py

--- Exercícios/ex018.py
+++ Exercícios/ex018.py
@@ -1,3 +1,7 @@
+'''
+Desafio 18: Faça um programa que leia um ângulo qualquer e 
+mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+'''
 from math import sin
 from math import cos
 from math import tan
