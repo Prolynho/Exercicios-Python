c6b10a82884280334471854f62bfb75dbd62a6aa
Proly <92342752+Prolynho@users.noreply.github.com>
2023-01-23 10:16:24 -0300

    Update ex030.py

--- Exercícios/ex030.py
+++ Exercícios/ex030.py
@@ -1,3 +1,6 @@
+'''
+Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+'''
 number = int(input('Insert a number: '))
 if number % 2 == 0:
     print('Your number is a pair!')
