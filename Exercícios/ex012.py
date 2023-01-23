d2f403ace25223955008408440e11e8aeb9ad10f
Proly <92342752+Prolynho@users.noreply.github.com>
2023-01-23 09:57:13 -0300

    Update ex012.py

--- Exercícios/ex012.py
+++ Exercícios/ex012.py
@@ -1,3 +1,7 @@
+'''
+Faça um algoritmo que leia o preço de um produto 
+e mostre seu novo preço, com 5% de desconto
+'''
 price = float(input('Product price: '))
 discountPrice = price - (price * 5/100)
 print('New price: {:.2f}'.format(discountPrice))
